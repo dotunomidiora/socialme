272d8bc6d8f7faadbf8e982cf64a40077bd533ca
Dotun <dotunomidiora@icloud.com>
2024-01-25 10:29:56 +0000

    update

--- socialme_frontend/src/components/Login.jsx
+++ socialme_frontend/src/components/Login.jsx
@@ -1,64 +1,57 @@
 import React from 'react';
-import {GoogleLogin, GoogleOAuthProvider} from '@react-oauth/google';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom';
 import shareVideo from '../assets/share.mp4';
 import logo from '../assets/logowhite.png';
 import { client } from '../client';
 import jwt_decode from 'jwt-decode'
 
+//import JWT from 'google-auth-library';
 const Login = () => {
-    const navigate = useNavigate();
-
-    const responseGoogle = (response) => {
-
-        const userResponse = jwt_decode(response.credential);
-
-        localStorage.setItem('user', JSON.stringify(userResponse));
-        const { name, sub, picture } = userResponse;
-
-        const doc = {
-            _id: sub,
-            _type: 'user',
-            userName: name,
-            image: picture
-        }
-        
-        client.createIfNotExists(doc).then(()=>{
-            navigate('/', {replace: true})
-        });
-
+  const navigate = useNavigate();
+  const responseGoogle = async(response) => {
+    localStorage.setItem('user', JSON.stringify(response));
+    console.log(response);
+    const decoded = jwt_decode(response.credential)
+    const { sub, name, picture } = decoded;
+    const doc = {
+      _id: sub,
+      _type: 'user',
+      userName: name,
+      image: picture,
     };
-
-    return (
-        <div className="flex justify-start items-center flex-col h-screen">
-            <div className=" relative w-full h-full">
-                <video
-                    src={shareVideo}
-                    type="video/mp4"
-                    loop
-                    controls={false}
-                    muted
-                    autoPlay
-                    className="w-full h-full object-cover"
-                />
-
-                <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
-                    <div className="p-5">
-                        <img src={logo} width="130px" alt='logo' />
-                    </div>
-
-                    <GoogleOAuthProvider
-                        clientId={`${process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID}`} className="shadow-2xl">
-                        <GoogleLogin
-                            onSuccess={responseGoogle}
-                            onFailure={responseGoogle}
-                            cookiePolicy="single_host_origin"
-                        />
-                    </GoogleOAuthProvider>
-                </div>
-            </div>
+    client.createIfNotExists(doc).then(() => {
+      navigate('/', { replace: true });
+    });
+  };
+  
+  return (
+    <div className='flex justify-start items-center flex-col h-screen'>
+      <div className='relativr w-full h-full'>
+        <video src ={shareVideo} type ='video/mp4'
+        loop
+        controls={false}
+        muted
+        autoPlay
+        className='w-full h-full object-cover' />
+      </div>
+      <div className='absolute flex flex-col justify-center items-center top-0 right- left-0 bottom-0 bg-blackOverlay'>
+        <div className='p=5'>
+          <img  src={logo} width="130px" alt='logo'/>
         </div>
-    );
-};
+        <div className='shadow-2x1'>
+          <GoogleOAuthProvider
+          clientId={`${process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID}`}>
+            <GoogleLogin
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy = 'single_host_origin' />
+          </GoogleOAuthProvider>
 
-export default Login;
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Login
