656c7b7654412a51efe2193d7d0f17e845f2f616
Dotun <dotunomidiora@icloud.com>
2024-01-25 03:20:35 +0000

    update

--- socialme_frontend/src/client.js
+++ socialme_frontend/src/client.js
@@ -5,7 +5,8 @@ export const client = sanityClient({
     projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
     apiVersion: '2021-11-16',
-    token: 'process.env.REACT_APP_SANITY_TOKEN'
+    token: process.env.REACT_APP_SANITY_TOKEN,
+    useCdn: true,
 });
 
 const builder = imageUrlBuilder(client);
--- socialme_frontend/src/components/Login.jsx
+++ socialme_frontend/src/components/Login.jsx
@@ -1,66 +1,64 @@
+import React from 'react';
+import {GoogleLogin, GoogleOAuthProvider} from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom';
-import { faGoogle } from 'react-icons/fc';
-import { GoogleOAuthProvider, GoogleLogin, googleLogout } from '@react-oauth/google'
 import shareVideo from '../assets/share.mp4';
 import logo from '../assets/logowhite.png';
+import { client } from '../client';
 import jwt_decode from 'jwt-decode'
 
-import { client } from '../client';
-
-
-
-
 const Login = () => {
-  const navigate = useNavigate();
-  const responseGoogle = (response) => {
-    //let's set them to local storage
-    localStorage.setItem('user', JSON.stringify(response.profileObj));
-   //time to deconstruct some properties
-    const { name, googleId, imageUrl} = response.profileObj;
-//transfer the Google user info to a Sanity document so it can be saved to the db
-//this doc contains the fields that we have specified for our Sanity user schema (user.js)
-const doc = {
-  _id: googleId,
-  _type: 'user',
-  userName: name,
-  image: imageUrl
-}
+    const navigate = useNavigate();
 
-client.createIfNotExists(doc)
-.then(() => {
-  navigate('/', {replace: true})
-})
-  }
+    const responseGoogle = (response) => {
+
+        const userResponse = jwt_decode(response.credential);
+
+        localStorage.setItem('user', JSON.stringify(userResponse));
+        const { name, sub, picture } = userResponse;
+
+        const doc = {
+            _id: sub,
+            _type: 'user',
+            userName: name,
+            image: picture
+        }
+        
+        client.createIfNotExists(doc).then(()=>{
+            navigate('/', {replace: true})
+        });
+
+    };
+
     return (
-        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
-            <div className='flex justify-start items-center flex-col h-screen'>
-                <div className='relative w-full h-full'>
-                    <video
-                        src={shareVideo}
-                        type="video/mp4"
-                        loop
-                        controls={false}
-                        muted
-                        autoPlay
-                        className="h-full w-full object-cover"
-                    />
-                    <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
-                        <div className='p-5'>
-                            <img src={logo} width="130px" alt='logo' />
-                        </div>
-                        <div className='shadow-2xl'>
-                            <GoogleLogin
+        <div className="flex justify-start items-center flex-col h-screen">
+            <div className=" relative w-full h-full">
+                <video
+                    src={shareVideo}
+                    type="video/mp4"
+                    loop
+                    controls={false}
+                    muted
+                    autoPlay
+                    className="w-full h-full object-cover"
+                />
+
+                <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
+                    <div className="p-5">
+                        <img src={logo} width="130px" alt='logo' />
+                    </div>
+
+                    <GoogleOAuthProvider
+                        clientId={`${process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID}`} className="shadow-2xl">
+                        <GoogleLogin
                             onSuccess={responseGoogle}
                             onFailure={responseGoogle}
                             cookiePolicy="single_host_origin"
-                            />
-
-                        </div>
-                    </div>
+                        />
+                    </GoogleOAuthProvider>
                 </div>
             </div>
-        </GoogleOAuthProvider>
-    )
-}
+        </div>
+    );
+};
 
-export default Login
+export default Login;
